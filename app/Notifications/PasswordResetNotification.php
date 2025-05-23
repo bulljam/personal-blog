e5594a60f771a08a20cb442dd8fb0dbd73e48084
Aymane Bouljam <235529830+bulljam@users.noreply.github.com>
2025-05-23 09:15:42 +0100

    feat: implement custom notification system for user alerts

---
+++ app/Notifications/PasswordResetNotification.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordResetNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Password Reset successful.')
+            ->greeting("Hello {$notifiable->name}!")
+            ->line('Your password has been successfuly reset.')
+            ->line('If you didn\'t request the change. Please change your password immediately.')
+            ->action('Sign In', route('login'))
+            ->line('Thank you for using our service!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
